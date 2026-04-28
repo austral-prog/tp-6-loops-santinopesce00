4883446b700e1abb7dd2732c3402bafe50a329ef
santinopesce00 <spesce@mail.austral.edu.ar>
2026-04-28 12:17:58 -0300

    Update list_modify.py

--- list_modify.py
+++ list_modify.py
@@ -12,7 +12,11 @@ def put(value, lst):
         put("Blue", colors) -> 1
         # colors ahora es ["Red", "Blue", "Green"]
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    for  i in range (len(lst)):
+        if lst[i] == "":
+            lst[i] = value
+            return i
+    return -1
 
 
 def remove(value, lst):
@@ -26,4 +30,9 @@ def remove(value, lst):
         remove("Red", colors) -> 2
         # colors ahora es ["", "Green", "", "Blue"]
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    contador_eliminados = 0
+    for i in range (len(lst)):
+        if lst[i] == value:
+            lst[i] = ""
+            contador_eliminados += 1
+    return contador_eliminados
